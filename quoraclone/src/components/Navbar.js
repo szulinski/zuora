d7fcc769852843244cdd799238131fab0f2f093c
Seth Z <szulinski917@gmail.com>
2022-07-17 21:35:18 -0400

    React modal online, req CSS fix

--- quoraclone/src/components/Navbar.js
+++ quoraclone/src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import titleImg from '../assets/zuoratitle.png';
-import { HomeOutlined, LanguageOutlined, AssignmentTurnedInOutlined, PeopleOutlined, CommentOutlined, NotificationsActiveOutlined, SearchOutlined } from '@mui/icons-material';
-import { Avatar, Button } from '@mui/material';
+import { HomeOutlined, LanguageOutlined, AssignmentTurnedInOutlined, PeopleOutlined, CommentOutlined, NotificationsActiveOutlined, SearchOutlined, PeopleAltOutlined, ExpandMore, LinkOutlined } from '@mui/icons-material';
+import { Avatar, Button, Input } from '@mui/material';
 import '../css/Navbar.css'
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import { auth } from '../Firebase';
+import Modal from 'react-modal'
 
 function Navbar() {
     const user = useSelector(selectUser)
+    const [openModal, setOpenModal] = useState(false);
 
     return (
         <div className = 'navHeader'>
@@ -43,9 +45,49 @@ function Navbar() {
                         <Avatar onClick = {() => auth.signOut()} src = {user.photo} />
                     </div>
                     <LanguageOutlined />
-                    <Button className = 'button'> 
+                    <Button className = 'button' onClick = {() => setOpenModal(true)}> 
                         <text className = 'text'> Add Question </text> 
                     </Button>
+                    <Modal
+                        className = 'modal' 
+                        isOpen = { openModal }
+                        onRequestClose = {() => setOpenModal(false)}
+                        
+                        >
+                        <div className = 'modal_title'>
+                            <h5>Add Question</h5>
+                            <h5>Share Link</h5>
+                            <div className = 'modal_info'>
+                                <Avatar
+                                className = 'modal_avatar'
+                                src = {user.photo}
+                                />
+                                <p>{ user.displayName ? user.displayName : user.email} asked </p>
+                                <div className = 'modal_scope'>
+                                    <PeopleAltOutlined />
+                                    <p>Public</p>
+                                    <ExpandMore />
+                                </div> 
+                            </div>
+                            <div className = 'modal_field'>
+                                <Input className = 'input'
+                                    type = "text"
+                                    placeholder = 'Start your question with "What", "How", "Why", etc'
+                                />   
+                                <div className = 'modal_field_link'>
+                                    <LinkOutlined />
+                                 <Input className = 'input'
+                                      type = "text"
+                                     placeholder = 'Optional: include a link that gives context'
+                                 />   
+                                </div>
+                            </div>
+                            <div className = 'modal_buttons'>
+                                <button type = 'submit' className = 'add'>Add Question</button>
+                            </div>
+                            <button onClick = {() => setOpenModal(false)}>Close</button>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
